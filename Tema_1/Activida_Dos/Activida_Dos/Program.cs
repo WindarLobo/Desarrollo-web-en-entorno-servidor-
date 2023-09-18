b3f26d4d8bb331e8af8051b9238611c8496e7507
WindarLoboGarces <windar.lobo.garces@ciclosmontecastelo.com>
2023-09-18 21:22:38 +0200

    Tema 1

---
+++ Tema_1/Activida_Dos/Activida_Dos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Activida_Dos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float numero;
+            float promedio;
+            float i = 0;
+            float suma = 0;
+            do
+            {
+                WriteLine("Introduce un  numero");
+                string usuario = Console.ReadLine();
+
+                if (float.TryParse(usuario, out numero))
+                {
+                    i++;
+                    suma += numero;
+                }
+                else
+                {
+                    WriteLine("Error al introducir");
+                }
+
+            } while (i < 5);
+
+
+            promedio = suma / 5;
+
+            WriteLine($"El promedio es: {promedio}");
+            ReadKey();
+
+        }
+    }
+}
---
+++ Tema_1/Actividad_Uno/Actividad_Uno/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Actividad_Uno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+            
+            ReadKey();
+
+        }
+    }
+}
