bdf04dca183e58efebb9e24b4fb0b92be6b726ab
WindaLobo <lobo.winsus@gmail.com>
2023-09-20 18:48:02 +0200

    Añadiendo ejercicios

--- Tema_1/Boletin_1/Ejercicios/Ejercicios/Ejercicio_1_/Ejercicio_12_/Program.cs
+++ Tema_1/Boletin_1/Ejercicios/Ejercicios/Ejercicio_1_/Ejercicio_12_/Program.cs
@@ -9,16 +9,25 @@ internal class Program
          D=√3*a
          AL=4*a2
           V=a3 */
-
-        WriteLine("Ingrese la longitud de un lado del cubo: ");
-        string datosIngresados = ReadLine();
-        if (double.TryParse(datosIngresados, out double ladoCubo))
+        do
         {
 
+            WriteLine("Ingrese la longitud de un lado del cubo: ");
+            string datosIngresados = ReadLine();
+            if (double.TryParse(datosIngresados, out double ladoCubo))
+            {
 
-            WriteLine($"\nDiagonal del cubo es : {Math.Sqrt(3) * ladoCubo}\nEl area lateral del cubo es : " +
-                $"{4 * Math.Pow(ladoCubo, 2)}\nVolumen del cubo es : {Math.Pow(ladoCubo, 3)}");
-            ReadLine();
-        }
+
+                WriteLine($"\nDiagonal del cubo es : {Math.Sqrt(3) * ladoCubo}\nEl area lateral del cubo es : " +
+                    $"{4 * Math.Pow(ladoCubo, 2)}\nVolumen del cubo es : {Math.Pow(ladoCubo, 3)}");
+                ReadLine();
+                break;
+            }
+            else
+            {
+                WriteLine("\nError al introducir la longitud de un lado del cubo");
+                ReadLine();
+            }
+        }while (true);
     }
 }
