2a196143f1e5ecd50c5beb6079e17c0d118ac464
WindaLobo <lobo.winsus@gmail.com>
2023-10-23 12:50:12 +0200

    Modificación en el nombre del boletin2 / tema2

---
+++ Tema_2/Tarea_tema2/LoboGarcesWindarTarea2/LoboGarcesWindarTarea2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoboGarcesWindarTarea2.Models;
+
+namespace LoboGarcesWindarTarea2
+{
+    public class HomeController : Controller
+    {
+       
+        [HttpGet]
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+
+        // Metodo de accion  Index(), que se  encarga de  manejar las solicitudes HTTP y devuelve  una viewModel a la vista.
+        public IActionResult Index()
+        {
+            var imagenRuta2 = "~/img/justin-lim-tloFnD-7EpI-unsplash.jpg";
+
+            var urlImagen2 = Url.Content(imagenRuta2);
+
+            var viewModel = new MiCurriculumViewModel
+
+            {
+
+                UrlImagenIndex = urlImagen2
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Tema_2/Tarea_tema2/LoboGarcesWindarTarea2/LoboGarcesWindarTarea2/Controllers/MiCurriculumController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using LoboGarcesWindarTarea2.Models;
+using System;
+using System.Diagnostics.Metrics;
+
+namespace LoboGarcesWindarTarea2.Controllers
+{
+    //MiCurriculum hereda de Controller 
+
+    public class MiCurriculumController : Controller
+    {
+        //Variable privada del tipo ILogger<MiCurriculumController>
+
+        private readonly ILogger<MiCurriculumController> _logger;
+
+
+        // Constructor del controlador que recibe una instancia del tipo ILogger<MiCurriculumController>
+
+        public MiCurriculumController(ILogger<MiCurriculumController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet] Solicitud GET a la ruta "/MiCurriculum".
+
+        [HttpGet]
+
+        // [Route("/MiCurriculum")].Cuando un usuario realiza una solicitud GET a esta ruta,se ejecutara y respondera a la solicitud..
+
+        [Route("/MiCurriculum")]
+
+        // Metodo de accion MiCurriculum(), prepara los datos necesarios para mostrar en la vista.
+        public IActionResult MiCurriculum()
+        {
+            var imagenRuta = "~/img/Windar.jpg";
+
+            var urlImagen = Url.Content(imagenRuta);
+
+            var viewModel = new MiCurriculumViewModel
+            {
+                Idiomas = ObtenerIdiomas(),
+
+                Intereses = ObtenerIntereses(),
+
+                Experiencias = ObtenerExperiencia(),
+
+                Formacion = ObtenerFormacion(),
+
+                Conocimientos = ObtenerConocimiento(),
+
+                UrlImagenCurriculum = urlImagen
+            };
+
+            return View(viewModel);
+        }
+
+        // Metodo ObtenerIdiomas(), que devuelve una lista de idioma.
+        private List<string> ObtenerIdiomas()
+        {
+            return new List<string> { "Español-nativo", "Gallego-medio", "Ingles-medio" };
+        }
+
+        //Metodo ObtenerIntereses(), que devuelve una lista de instereses.
+        private List<string> ObtenerIntereses()
+        {
+            return new List<string> { "Natacion", "Actividad al aire libre", "Correr", "Cine" };
+        }
+
+        //Metodo ObtenerExperiencia(),esta función crea y devuelve una lista de experiencias laborales con información específica sobre cada experiencia.
+        private List<ExperienciaViewModel> ObtenerExperiencia()
+        {
+            return new List<ExperienciaViewModel>
+            {
+               new ExperienciaViewModel
+               {
+                       Titulo = "BritishAmericanTobacco ",
+
+                       Descripciones =  new List<string> {"Representante de ventas"},
+
+                       Inicio = new DateTime(2020,11,5),
+
+                       Fin= null
+               },
+
+                new ExperienciaViewModel {
+
+                    Titulo = "El Cortes Ingles (CH Carolina Herrera)",
+
+                    Descripciones= new List<string> {"Control de cobros.\n Stock.\nReporte de resultados." },
+
+                    Inicio = new DateTime(2019,11,25),
+
+                    Fin=new DateTime(2020,10,1)},
+
+               new ExperienciaViewModel {
+                   Titulo = "Laboratorio Clínico (La Medalla Milagrosa)",
+
+                   Descripciones= new List<string> {"Atención al cliente.\nControl de pagos. \nControl de cobros." },
+
+                   Inicio = new DateTime(2016,7,30),
+
+                   Fin=new DateTime(2017,4,15)
+               },
+
+
+            };
+
+        }
+
+        //Metodo ObtenerFormacion(),crea y devuelve una lista de formaciones educativas.
+        private List<FormacionViewModel> ObtenerFormacion()
+        {
+
+            return new List<FormacionViewModel>
+            {
+               new FormacionViewModel
+               {
+                       Titulo = "Ciclo superior desarrollo de aplicaciones web. ",
+
+                       NombreInstituto =  "Centro de Formación Profesional Daniel Castelao.",
+
+                       Inicio = new DateTime(2022,11,5),
+
+                       Fin=null
+               },
+
+                new FormacionViewModel
+               {
+                       Titulo = "Ingeniera en Gas Natural ",
+
+                       NombreInstituto =  "Universidad Rafael María Baralt",
+
+                       Inicio = new DateTime(2010,11,5),
+
+                       Fin= new DateTime(2015,12,18)
+               },
+            };
+        }
+
+        //Metodo ObtenerConocimiento(), crea y devuelve una lista los conocimientos. 
+        private List<ConocimientoViewModel> ObtenerConocimiento()
+        {
+            return new List<ConocimientoViewModel>
+            {
+                 new ConocimientoViewModel
+                {
+                      Nombre = " Word ",
+                },
+
+                 new ConocimientoViewModel
+                 {
+                     Nombre = "Mail ",
+                 },
+
+                 new ConocimientoViewModel
+                 {
+                     Nombre = "Excell ",
+                 },
+
+                 new ConocimientoViewModel
+                 {
+                     Nombre = "SQL Server  ",
+                 },
+
+                  new ConocimientoViewModel
+                 {
+                      Nombre = "Javascript ",
+                 },
+
+            };
+        }
+
+
+
+    }
+}
+
