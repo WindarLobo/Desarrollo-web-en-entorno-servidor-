6063424693da568da056d7a7e8fc962beb0889dc
WindarLoboGarces <windar.lobo.garces@ciclosmontecastelo.com>
2023-12-01 15:51:33 +0100

    ultimos ajustes

--- Tema_4/Tarea4/LoboGarcesWindarTarea4/LoboGarcesWindarTarea4/Program.cs
+++ Tema_4/Tarea4/LoboGarcesWindarTarea4/LoboGarcesWindarTarea4/Program.cs
@@ -4,49 +4,47 @@ using LoboGarcesWindarTarea4.DataBase.Modelo;
 using LoboGarcesWindarTarea4.DataBase.Repository;
 using System.Drawing;
 
-namespace LoboGarcesWindarTarea4
+namespace LoboGarcesWindarTarea4;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+       
+       
+      var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        //Cadena de conexion 
+
+        builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("ConexionMontecastelo")));
+
+        //Registro de interfaz 
+        builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-           
-           
-          var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            //Cadena de conexion 
-
-            builder.Services.AddSingleton(new Conexion(builder.Configuration.GetConnectionString("ConexionMontecastelo")));
-
-            //Registro de interfaz 
-            builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            // Utilizo esta funcion app.MapControllers() para gestionar las ruta mediante atributos.
-            app.MapControllers();
-
-            app.Run();
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
-    }
 
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        // Utilizo esta funcion app.MapControllers() para gestionar las ruta mediante atributos.
+        app.MapControllers();
+
+        app.Run();
+    }
 }
--- Tema_4/Tarea4/LoboGarcesWindarTarea4/LoboGarcesWindarTarea4/SimuladorCombate.cs
+++ Tema_4/Tarea4/LoboGarcesWindarTarea4/LoboGarcesWindarTarea4/SimuladorCombate.cs
@@ -1,167 +1,166 @@
 ﻿using LoboGarcesWindarTarea4.DataBase.Modelo;
 
 
-namespace LoboGarcesWindarTarea4.Models
+namespace LoboGarcesWindarTarea4.Models;
+
+public class SimuladorCombate
 {
-    public class SimuladorCombate
+    //RealizarCombate() se encarga de la simulación de combates entre Pokémon de dos equipos. 
+    public static Batalla RealizarCombate(Equipo equipo1, Equipo equipo2)
     {
-        //RealizarCombate() se encarga de la simulación de combates entre Pokémon de dos equipos. 
-        public static Batalla RealizarCombate(Equipo equipo1, Equipo equipo2)
+       
+        var batalla = new Batalla()
         {
-           
-            var batalla = new Batalla()
-            {
-                Equipo1 = equipo1,
+            Equipo1 = equipo1,
 
-                Equipo2 = equipo2,
+            Equipo2 = equipo2,
 
-                Combates = new List<Combate>()
-            };
+            Combates = new List<Combate>()
+        };
 
-            for (int i = 0; i < equipo1.Pokemons.Count; i++)
-            {
-                
-                var pokemon1 = equipo1.Pokemons[i];
-
-                var pokemon2 = equipo2.Pokemons[i];
-
-                var combate = new Combate
-                {
-                    Pokemon1 = pokemon1,
-
-                    Pokemon2 = pokemon2,
-
-                    Ganador = DeterminarGanadorCombate(pokemon1, pokemon2)
-                };
-
-                batalla.Combates.Add(combate);
-                
-            }
-
-            return batalla;
-        }
-
-        private static PokemonGanador DeterminarGanadorCombate(Pokemon pokemon1, Pokemon pokemon2)
+        for (int i = 0; i < equipo1.Pokemons.Count; i++)
         {
             
-            // Lógica de comparación de tipos
+            var pokemon1 = equipo1.Pokemons[i];
 
-            // Implementa según tus reglas específicas
+            var pokemon2 = equipo2.Pokemons[i];
 
-            var pokemon1Fuerte = false;
-
-            var pokemon2Fuerte = false;
-
-            foreach (var tipo1 in pokemon1.Tipos)
+            var combate = new Combate
             {
-                foreach (var tipo2 in pokemon2.Tipos)
-                {
-                    if (EsTipoFuerte(tipo1, tipo2))
-                    {
-                        pokemon1Fuerte = true;
-                       
-                        break;
-                    }
-                }
-            }
+                Pokemon1 = pokemon1,
 
-            foreach (var tipo2 in pokemon2.Tipos)
-            {
-                foreach (var tipo1 in pokemon1.Tipos)
-                {
-                    if (EsTipoFuerte(tipo2, tipo1))
-                    {
-                        pokemon2Fuerte = true;
-                        break;
-                    }
-                }
-            }
+                Pokemon2 = pokemon2,
 
-            if (pokemon1Fuerte && !pokemon2Fuerte)
-            {
-                return new PokemonGanador
-                {
-                    Pokemon = pokemon1,
+                Ganador = DeterminarGanadorCombate(pokemon1, pokemon2)
+            };
 
-                    Motivo = "Es mas fuerte por tipo. "
-
-                    
-                };
-            }
-            else if (pokemon2Fuerte && !pokemon1Fuerte)
-            {
-                return new PokemonGanador
-                {
-                    Pokemon = pokemon2,
-
-                    Motivo = "Es mas fuerte por tipo.",
-
-                   
-                };
-            }
-            else
-            {
-                // En caso de empate en tipos, se suma   peso y altura para determinar la masa
-
-                if (pokemon1.Peso + pokemon1.Altura > pokemon2.Peso + pokemon2.Altura)
-
-                    return new PokemonGanador { Pokemon = pokemon1, Motivo = "Por  volume de masa"};
-
-                else
-                {
-                    return new PokemonGanador { Pokemon = pokemon2, Motivo = "Por volume de masa"};
-                }
-
-            }
-           
+            batalla.Combates.Add(combate);
+            
         }
 
+        return batalla;
+    }
 
-        // Lógica para determinar si tipo1 es fuerte contra tipo2
+    private static PokemonGanador DeterminarGanadorCombate(Pokemon pokemon1, Pokemon pokemon2)
+    {
+        
+        // Lógica de comparación de tipos
 
         // Implementa según tus reglas específicas
-        private static bool EsTipoFuerte(Tipo tipo1, Tipo tipo2)
 
-        {  //switch c# un switch nuevo 
+        var pokemon1Fuerte = false;
 
-            return tipo1.TipoNombre switch
+        var pokemon2Fuerte = false;
+
+        foreach (var tipo1 in pokemon1.Tipos)
+        {
+            foreach (var tipo2 in pokemon2.Tipos)
             {
-                "Fuego" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Acero",
+                if (EsTipoFuerte(tipo1, tipo2))
+                {
+                    pokemon1Fuerte = true;
+                   
+                    break;
+                }
+            }
+        }
 
-                "Agua" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Tierra",
+        foreach (var tipo2 in pokemon2.Tipos)
+        {
+            foreach (var tipo1 in pokemon1.Tipos)
+            {
+                if (EsTipoFuerte(tipo2, tipo1))
+                {
+                    pokemon2Fuerte = true;
+                    break;
+                }
+            }
+        }
 
-                "Planta" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Tierra",
+        if (pokemon1Fuerte && !pokemon2Fuerte)
+        {
+            return new PokemonGanador
+            {
+                Pokemon = pokemon1,
 
-                "Eléctrico" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Volador",
+                Motivo = "Es mas fuerte por tipo. "
 
-                "Psíquico" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Volador",
-
-                "Hielo" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Tierra" || tipo2.TipoNombre == "Volador" || tipo2.TipoNombre == "Dragón",
-
-                "Volador" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Lucha" || tipo2.TipoNombre == "Bicho",
-
-                "Tierra" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Eléctrico" || tipo2.TipoNombre == "Acero" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Veneno",
-
-                "Lucha" => tipo2.TipoNombre == "Normal",
-
-                "Dragón" => tipo2.TipoNombre == "Dragón",
-
-                "Veneno" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Hada",
-
-                "Roca" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Hielo" || tipo2.TipoNombre == "Volador" || tipo2.TipoNombre == "Bicho",
-
-                "Bicho" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Siniestro" || tipo2.TipoNombre == "Planta",
-
-                "Fantasma" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Fantasma",
-
-                "Siniestro" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Fantasma",
-
-                "Hada" => tipo2.TipoNombre == "Lucha" || tipo2.TipoNombre == "Dragón" || tipo2.TipoNombre == "Siniestro",
-
-                "Acero" => tipo2.TipoNombre == "Hielo" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Hada",
-
-                _ => false,
+                
             };
         }
+        else if (pokemon2Fuerte && !pokemon1Fuerte)
+        {
+            return new PokemonGanador
+            {
+                Pokemon = pokemon2,
+
+                Motivo = "Es mas fuerte por tipo.",
+
+               
+            };
+        }
+        else
+        {
+            // En caso de empate en tipos, se suma   peso y altura para determinar la masa
+
+            if (pokemon1.Peso + pokemon1.Altura > pokemon2.Peso + pokemon2.Altura)
+
+                return new PokemonGanador { Pokemon = pokemon1, Motivo = "Por  volume de masa"};
+
+            else
+            {
+                return new PokemonGanador { Pokemon = pokemon2, Motivo = "Por volume de masa"};
+            }
+
+        }
+       
+    }
+
+
+    // Lógica para determinar si tipo1 es fuerte contra tipo2
+
+    // Implementa según tus reglas específicas
+    private static bool EsTipoFuerte(Tipo tipo1, Tipo tipo2)
+
+    {  //switch c# un switch nuevo 
+
+        return tipo1.TipoNombre switch
+        {
+            "Fuego" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Acero",
+
+            "Agua" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Tierra",
+
+            "Planta" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Tierra",
+
+            "Eléctrico" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Volador",
+
+            "Psíquico" => tipo2.TipoNombre == "Agua" || tipo2.TipoNombre == "Volador",
+
+            "Hielo" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Tierra" || tipo2.TipoNombre == "Volador" || tipo2.TipoNombre == "Dragón",
+
+            "Volador" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Lucha" || tipo2.TipoNombre == "Bicho",
+
+            "Tierra" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Eléctrico" || tipo2.TipoNombre == "Acero" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Veneno",
+
+            "Lucha" => tipo2.TipoNombre == "Normal",
+
+            "Dragón" => tipo2.TipoNombre == "Dragón",
+
+            "Veneno" => tipo2.TipoNombre == "Planta" || tipo2.TipoNombre == "Hada",
+
+            "Roca" => tipo2.TipoNombre == "Fuego" || tipo2.TipoNombre == "Hielo" || tipo2.TipoNombre == "Volador" || tipo2.TipoNombre == "Bicho",
+
+            "Bicho" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Siniestro" || tipo2.TipoNombre == "Planta",
+
+            "Fantasma" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Fantasma",
+
+            "Siniestro" => tipo2.TipoNombre == "Psíquico" || tipo2.TipoNombre == "Fantasma",
+
+            "Hada" => tipo2.TipoNombre == "Lucha" || tipo2.TipoNombre == "Dragón" || tipo2.TipoNombre == "Siniestro",
+
+            "Acero" => tipo2.TipoNombre == "Hielo" || tipo2.TipoNombre == "Roca" || tipo2.TipoNombre == "Hada",
+
+            _ => false,
+        };
     }
 }
